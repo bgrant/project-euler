9bbc33d139c39183aa8aa97ae6e794ebd5eb74e4
Robert David Grant <robert.david.grant@gmail.com>
2011-11-12 18:54:04 -0600

    Add problem 345.
    Works, but not fast enough.  Doesn't take advantage of optimal substructure.

--- python/euler.py
+++ python/euler.py
@@ -340,7 +340,6 @@ def pandigitals(n):
     for x in permutations(digits[len(digits)-n:]):
         yield cton(x)
 
-
 ################
 ### Problems ###
 ################
@@ -651,6 +650,45 @@ def problem56(ulimit=100):
     return max(sum(digits(x**y)) for x in xrange(1,ulimit) 
                                  for y in xrange(1,ulimit))
 
+def problem345_impl0(m):
+    """Find the Matrix Sum of matrix `m`.
+
+    The Matrix Sum of a matrix m is defined as the maximum sum of matrix
+    elements in m such that each element is the only element is its own
+    row and column.
+
+    Not fast enough.
+    """
+    nrows, ncols = m.shape
+    rows = array(range(nrows))
+    pcols = (array(p) for p in permutations(xrange(ncols)))
+    return max(m[rows, cols].sum() for cols in pcols)
+
+def problem345_impl1(m):
+    """Find the Matrix Sum of matrix `m`.
+
+    The Matrix Sum of a matrix m is defined as the maximum sum of matrix
+    elements in m such that each element is the only element is its own
+    row and column.
+
+    TODO: Not yet fast enough.
+    """
+    nrows, ncols = m.shape
+    rows = array(range(nrows))
+    pcols = (array(p) for p in permutations(xrange(ncols)))
+    nsums = factorial(ncols)
+    count = 0
+    cached_max = 0
+    skip = 100000
+    for cols in pcols:
+        current_sum = m[rows, cols].sum()
+        if current_sum > cached_max:
+            cached_max = current_sum
+        if divides(skip, count):
+            print count / nsums * 100, "% Done"
+        count += 1
+
+
 ############
 ### Data ###
 ############
@@ -817,3 +855,27 @@ p18_data = (\
     (63,66,4,68,89,53,67,30,73,16,69,87,40,31),
     (4,62,98,27,23,9,70,98,73,93,38,53,60,4,23),
     (None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None))
+
+p345_test_data = array([
+    [  7, 53,183,439,863],
+    [497,383,563, 79,973],
+    [287, 63,343,169,583],
+    [627,343,773,959,943],
+    [767,473,103,699,303]])
+
+p345_data = array([
+    [  6, 53,183,439,863,497,383,563, 79,973,287, 63,343,169,583],
+    [627,343,773,959,943,767,473,103,699,303,957,703,583,639,913],
+    [447,283,463, 29, 23,487,463,993,119,883,327,493,423,159,743],
+    [217,623,  3,399,853,407,103,983, 89,463,290,516,212,462,350],
+    [960,376,682,962,300,780,486,502,912,800,250,346,172,812,350],
+    [870,456,192,162,593,473,915, 45,989,873,823,965,425,329,803],
+    [973,965,905,919,133,673,665,235,509,613,673,815,165,992,326],
+    [322,148,972,962,286,255,941,541,265,323,925,281,601, 95,973],
+    [445,721, 11,525,473, 65,511,164,138,672, 18,428,154,448,848],
+    [414,456,310,312,798,104,566,520,302,248,694,976,430,392,198],
+    [184,829,373,181,631,101,969,613,840,740,778,458,284,760,390],
+    [821,461,843,513, 17,901,711,993,293,157,274, 94,192,156,574],
+    [ 34,124,  4,878,450,476,712,914,838,669,875,299,823,329,699],
+    [815,559,813,459,522,788,168,586,966,232,308,833,251,631,107],
+    [813,883,451,509,615, 77,281,613,459,205,380,274,302, 35,805]])
