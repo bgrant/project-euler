974a1286358fba9a73d332ea294df3cba8e9890f
Robert David Grant <robert.david.grant@gmail.com>
2015-06-20 13:01:28 -0700

    Add Cython solutions.

---
+++ cython/euler.py
@@ -0,0 +1,8 @@
+from _euler import *
+
+print "Problem 1:", problem1(1000)
+print "Problem 2:", problem2(int(4e6))
+print "Problem 4:", problem4()
+print "Problem 5:", problem5()
+print "Problem 6:", problem6(100)
+print "Problem 8:", problem8()
